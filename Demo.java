86e8140b14f883e7b0e13c96dad09307d12465c3
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-02-10 14:59:43 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,19 @@
+package com.tka;
+
+public class Demo {
+
+	public void m1(int a ) {
+
+	}
+
+	public void m1(long b) {
+		
+	}
+
+	public static void main(String[] args) {
+
+		Demo sd = new Demo();
+		sd.m1(1);
+		sd.m1(1);
+	}
+}
